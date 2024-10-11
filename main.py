0e8eac8bcdf6c289ab5c4de4facdba9accc570d9
sirvx <Artem.Kravchuk@infiz.khpi.edu.ua>
2024-10-11 14:19:30 +0300

    Реалізован клас для поступового напису, оновлено документацію

--- main.py
+++ main.py
@@ -9,34 +9,41 @@ window = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
 font1 = font.Font(None, 40)
 
-
-start_time = t.time()
 text_list = 'Hello World!'
-x = 0
-i=0
 
 
-def print_text(text):
-    global start_time, x, i
-    new_time = t.time()
-    if new_time - start_time > 0.1:
-        if i < len(text):
-            text = font1.render(f'{text[i]}', True, (255, 255, 255))
-            text_rect = text.get_rect()
-            window.blit(text, (x, 200))
-            x += text_rect.width
-            i += 1
-            start_time = t.time()
+class TextPrinter:
+    def __init__(self, text, font, speed=0.1, start_x=0, start_y=200):
+        self.text = text
+        self.font = font
+        self.speed = speed
+        self.x = start_x
+        self.y = start_y
+        self.index = 0
+        self.start_time = t.time()
+
+    def print_text(self, surface):
+        new_time = t.time()
+        if new_time - self.start_time > self.speed:
+            if self.index < len(self.text):
+                letter = self.font.render(f'{self.text[self.index]}', True, (255, 255, 255))
+                letter_rect = letter.get_rect()
+                surface.blit(letter, (self.x, self.y))
+                self.x += letter_rect.width
+                self.index += 1
+                self.start_time = t.time()
 
 
+printer = TextPrinter(text_list, font1)
+test = TextPrinter('test1', font1, start_x=200, start_y=100)
 
 while True:
     for e in event.get():
         if e.type == QUIT:
             quit()
 
-    #window.fill((0, 0, 0))
-    print_text('Hello World!')
+    printer.print_text(window)
+    test.print_text(window)
 
     display.update()
     clock.tick(60)
